991e06ff3fd243fa742ff35bfd35fce861d2e9b9
dhiaaziz <azizwkwkwk@gmail.com>
2021-09-08 22:44:03 +0700

    progress jsonManipulation 80%

--- jsonManipulation/main.js
+++ jsonManipulation/main.js
@@ -6,17 +6,32 @@ let data = JSON.parse(rawdata);
 
 
 
-//No
-function findByType(arrOfObject, type) {
+//done
+function findByType(arrOfObject, searchType) {
     const filtered = arrOfObject.filter((obj) => {
-        // return obj.type === type
-        return obj['type'] === type
-
+        // return obj.type === searchType
+        return obj['type'] === searchType
     })
     // return jsonData.map(obj => Object.values(obj))
     return filtered
 }
 
+function findByTag(arrOfObject, searchTag) {
+    const filtered = arrOfObject.filter(obj => {
+        return obj['tags'].includes(searchTag)
+    })
+    return filtered
+}
+
+function findByPlacementName(arrOfObject, searchPlacementName) {
+    const filtered = arrOfObject.filter((obj) => {
+        return obj['placement']['name'] == searchPlacementName
+    })
+    return filtered
+}
+
+
+//undone
 function findByPurchasedAt(arrOfObject){
     const filtered = arrOfObject.filter((obj) => {
         return obj['purchased_at'] === 1579190471
@@ -24,17 +39,28 @@ function findByPurchasedAt(arrOfObject){
     return filtered
 }
 
+
+
 // let test = data.filter((d) => d)
 
 // console.log(data[1].inventory_id);
 // console.log(test)
 // console.log(Object.keys(data[1]))
 
+//done
 let filteredFurniture = findByType(data, 'furniture')
 let filteredElectronic = findByType(data, 'electronic')
-let filteredInJune= findByPurchasedAt(data)
+let filteredBrownTags = findByTag(data, 'brown')
+let filteredMeetingRoom = findByPlacementName(data, 'Meeting Room')
 
-var someDate = new Date(dateString);
-someDate = someDate.getTime();
+
+
+
+
+//undone
+let filteredInJune = findByPurchasedAt(data)
+
+// var someDate = new Date(dateString);
+// someDate = someDate.getTime();
 
 console.log(filteredInJune);
